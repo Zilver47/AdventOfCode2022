3cf65b6934ffe6e2aa67095eefbe11b34d8887d0
ag-intercept <ageertsema@intercept.cloud>
2022-12-03 07:32:17 +0100

    Day 03

---
+++ Day03/Day03.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Day03 : IChallenge
+{
+    private readonly string[] _input;
+
+    public Day03(string[] input)
+    {
+        _input = input;
+    }
+
+    public long Part1()
+    {
+        var lines = _input.ToList();
+
+        long score = lines.Sum(l => Priority(FindCommon(l)));
+
+        return score;
+    }
+
+    private static int Priority(char value)
+    {
+        if (char.IsLower(value))
+        {
+            return value - 96;
+        }
+
+        return value - 38;
+    }
+
+    private static char FindCommon(string value)
+    {
+        var first = value[..(value.Length / 2)];
+        var second = value[(value.Length / 2)..];
+        var intersect = first.Intersect(second);
+        return intersect.First();
+    }
+
+    public long Part2()
+    {
+        var lines = _input.ToList();
+
+        var group = new List<string>();
+        long score = 0;
+        for (var i = 0; i < lines.Count; i++)
+        {
+            group.Add(lines[i]);
+
+            if ((i+1) % 3 == 0)
+            {
+                score += Priority(FindCommon2(group));
+                group.Clear();
+            }
+        }
+
+        return score;
+    }
+
+    private static char FindCommon2(IReadOnlyList<string> value)
+    {
+        var intersect = value[0].Intersect(value[1]).Intersect(value[2]);
+        return intersect.First();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
 var timer = new Stopwatch();
 timer.Start();
 
-var lines = File.ReadAllLines($"{nameof(Day02)}\\input.txt");
-var generator = new Day02(lines);
+var lines = File.ReadAllLines($"{nameof(Day03)}\\input.txt");
+var generator = new Day03(lines);
 
 Console.WriteLine($"--- {generator.GetType().Name} ---");
 Console.Write("Answer 1: ");
